930d830e4ffff31fb666dddee984e1b635213e50
furkanfatihkok <furkankok296@gmail.com>
2024-06-10 03:15:53 +0300

    finished detailVC UI

---
+++ Kiwix/Screens/Detail/Cells/ButtonCell/ButtonCell.swift
@@ -0,0 +1,19 @@
+//
+//  ButtonCell.swift
+//  Kiwix
+//
+//  Created by FFK on 10.06.2024.
+//
+
+import UIKit
+
+final class ButtonCell: UICollectionViewCell {
+
+    static let identifier = "ButtonCell"
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
--- Kiwix/Screens/Detail/DetailViewController.swift
+++ Kiwix/Screens/Detail/DetailViewController.swift
@@ -17,6 +17,7 @@ final class DetailViewController: UIViewController {
     
     @IBOutlet weak var headetView: HeaderView!
     @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var collectionView: UICollectionView!
     
     var words = [Word]()
     
@@ -26,8 +27,9 @@ final class DetailViewController: UIViewController {
         
         tableView.register(UINib(nibName: WordCell.identifier, bundle: nil), forCellReuseIdentifier: WordCell.identifier)
         tableView.register(UINib(nibName: CustomSection.identifier, bundle: nil), forHeaderFooterViewReuseIdentifier: CustomSection.identifier)
+        collectionView.register(UINib(nibName: ButtonCell.identifier, bundle: nil), forCellWithReuseIdentifier: ButtonCell.identifier)
     }
-
+    
 }
 
 //MARK: - DetailViewProtocol
@@ -60,5 +62,23 @@ extension DetailViewController: UITableViewDelegate, UITableViewDataSource {
     
     func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
         return 50
+    }
+}
+
+// MARK: - UICollectionViewDelegate & UICollectionViewDataSource
+
+extension DetailViewController: UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        1
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell =  collectionView.dequeueReusableCell(withReuseIdentifier: ButtonCell.identifier, for: indexPath) as! ButtonCell
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: view.frame.width, height: 50)
     }
 }
